50e5851ebae38e7cf9ca5f660d1d1bf53b9f836b
Ryan Daley <rdaley321@yahoo.com>
2018-10-18 09:40:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,9 +2,9 @@ var playlist = {
   drake: 'hotline bling'
 }
 
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle
-  return playlist
+function updatePlaylist(obj, artistName, songTitle) {
+  obj.artistName = songTitle
+  return obj
 }
 
 function removeFromPlaylist(playlist, artistName) {
